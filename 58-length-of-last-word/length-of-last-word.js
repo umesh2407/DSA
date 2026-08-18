334dfe8e15ef5eb53582923ee7052f591ecf5dbd
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-18 20:04:37 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (36.90%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -6,20 +6,15 @@ var lengthOfLastWord = function (s) {
     let n = s.length - 1;
     let count = 0;
     while (n >= 0) {
-        if (s[n] === " ") {
-            --n;
-        } else {
-            break;
-        }
-    }
-    while (n >= 0) {
         if (s[n] !== " ") {
             count++;
             n--;
-        } else {
+        } else if (s[n] === " " && count > 0) {
             break;
+        }
+        else {
+            n--;
         }
     }
     return count;
-
 };

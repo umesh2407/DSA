1ffc13f44fb0dacbe36a5d6fcd41502f9192ea8c
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-21 20:23:56 +0530

    Time: 1 ms (13.02%) | Memory: 53.6 MB (60.36%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -3,5 +3,15 @@
  * @return {string}
  */
 var toLowerCase = function (s) {
-    return s.toLowerCase();
+    let result = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            result += String.fromCharCode(s[i].charCodeAt(0) + 32);
+        } else {
+            result += s[i];
+        }
+    }
+
+    return result;
 };

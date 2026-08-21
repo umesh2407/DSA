1976fbda46f3e25db388861f3edc3d9b84f6dc4b
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-21 10:23:19 +0530

    Time: 1 ms (26.18%) | Memory: 54.2 MB (30.37%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -4,13 +4,29 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function (word1, word2) {
-    let w1 = "";
-    let w2 = "";
-    for (let i = 0; i < word1.length; i++) {
-        w1 = w1 + word1[i];
+    let i = 0;
+    let j = 0;
+    let p1 = 0;
+    let p2 = 0;
+
+    while (i < word1.length && j < word2.length) {
+        if (word1[i][p1] !== word2[j][p2]) {
+            return false;
+        }
+
+        p1++;
+        p2++;
+
+        if (p1 === word1[i].length) {
+            i++;
+            p1 = 0;
+        }
+
+        if (p2 === word2[j].length) {
+            j++;
+            p2 = 0;
+        }
     }
-    for (let j = 0; j < word2.length; j++) {
-        w2 = w2 + word2[j];
-    }
-    return w1 === w2;
+
+    return i === word1.length && j === word2.length;
 };

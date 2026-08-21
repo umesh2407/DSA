bd585f71559a58e8e6c22271925bdf3caac0a38f
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-21 09:19:15 +0530

    Time: 50 ms (11.60%) | Memory: 69.3 MB (18.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -5,14 +5,25 @@
 var groupAnagrams = function (strs) {
     let map = {};
     for (let i = 0; i < strs.length; i++) {
-        let sortedWord = strs[i].split("").sort().join("");
-        if (!map[sortedWord]) {
-            map[sortedWord] = [strs[i]];
+        //creating hashkey
+        let freqArr = Array(26).fill(0);
+        let s = strs[i];
+        for (let j = 0; j < s.length; j++) {
+            let index = s[j].charCodeAt() - 'a'.charCodeAt();
+            ++freqArr[index];
+        }
+        let key = "";
+        for (let k = 0; k < 26; k++) {
+            key = key + "#" + freqArr[k];
+        }
+
+        //filling map
+        if (!map[key]) {
+            map[key] = [s];
         } else {
-            map[sortedWord].push(strs[i]);
+            map[key].push(s);
         }
     }
 
     return [...Object.values(map)];
-
 };

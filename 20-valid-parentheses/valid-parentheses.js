7ed7fe548a43695637e1b9790f491b705fcdb42e
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-25 17:27:56 +0530

    Time: 4 ms (51.45%) | Memory: 55.1 MB (72.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -4,12 +4,17 @@
  */
 var isValid = function (s) {
     let stack = [];
+    let map = {
+        "[": "]",
+        "{": "}",
+        "(": ")"
+    }
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "[" || s[i] === "{" || s[i] === "(") {
+        if (map[s[i]]) {
             stack.push(s[i]);
         } else {
             let top = stack.pop();
-            if (!top || (top === "[" && s[i] != "]") || (top === "{" && s[i] != "}") || (top === "(" && s[i] != ")")) {
+            if (!top || s[i] !== map[top]) {
                 return false;
             }
         }

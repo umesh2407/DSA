99841e63eb1b66d578b4b62421e58aacd1ab2047
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-26 16:54:06 +0530

    Time: 2 ms (86.68%) | Memory: 58 MB (9.44%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -5,17 +5,18 @@
 var removeOuterParentheses = function (s) {
     let stack = [];
     let ans = "";
+    let count = 0;
     for (let i = 0; i < s.length; i++) {
         if (s[i] === "(") {
-            stack.push(s[i]);
-            if (stack.length > 1) {
+            count++;
+            if (count > 1) {
                 ans += s[i];
             }
         } else {
-            if (stack.length > 1) {
+            if (count > 1) {
                 ans += s[i];
             }
-            stack.pop();
+            count--;
         }
     }
 

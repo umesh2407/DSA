eb86f1e92e74b02f83947917eec48bbf9db11bfc
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-08-30 17:45:24 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (74.24%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} timer
+ * @return {string}
+ */
+var trafficSignal = function (timer) {
+    if (timer === 0) {
+        return "Green";
+    } else if (timer === 30) {
+        return "Orange";
+    } else if (timer > 30 && timer <= 90) {
+        return "Red";
+    } else {
+        return "Invalid";
+    }
+};

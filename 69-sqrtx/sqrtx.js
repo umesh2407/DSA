4b71f931f139c44f19a37cfa3e22a1b96ab6c406
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-09-07 22:05:12 +0530

    Time: 1 ms (78.96%) | Memory: 55.2 MB (86.80%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if (x < 2) return x;
+    let l = 2;
+    let r = Math.floor(x / 2);
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (x === m*m) {
+            return m;
+        } else if (x < m*m) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+    return r;
+};

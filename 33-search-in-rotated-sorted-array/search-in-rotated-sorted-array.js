572498d567d99ef3e1b89a577e6e5c5e1c220467
Umesh Choudhary <121879925+umesh2407@users.noreply.github.com>
2026-09-08 22:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (6.54%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,29 +4,34 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    let p = 0;
-    let q = 1;
-    while (nums[p] < nums[q]) {
-        p++;
-        q++;
-    }
-    if (target < nums[0]) {
-        p++;
-        q = nums.length - 1;
-    } else {
-        p = 0;
-    }
-    // Binary search
-    while (p <= q) {
-        let m = p + Math.floor((q - p) / 2);
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+
         if (nums[m] === target) {
             return m;
-        } else if (target > nums[m]) {
-            p = m + 1;
-        } else {
-            q = m - 1;
+        }
+
+        // Left half is sorted
+        if (nums[l] <= nums[m]) {
+            if (nums[l] <= target && target < nums[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        // Right half is sorted
+        else {
+            if (nums[m] < target && target <= nums[r]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
         }
     }
-    return -1;
 
+    return -1;
 };
